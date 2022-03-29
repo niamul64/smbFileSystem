4f2f6a9d12ff37c480439adf2966ae4d5306f954
Niamul Hasan <48917799+niamul64@users.noreply.github.com>
2022-03-30 02:19:33 +0600

    just grabing and showing the files in the browser

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # smbFileSystem
+
+1. Read Local Directory and List Files in PHP: https://www.youtube.com/watch?v=QAPZUNHQoNo&t=714s
--- index.php
+++ index.php
@@ -4,22 +4,39 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>file System</title>
 </head>
 <body>
-
-
 <?php
-include "smb.php";
 
-$localDir= '/home/user/Documents/sharef';
-$files= scandir($localDir);
-$files= array_diff($files,array('.','..'));
-$files= array_values($files);
+define("rootDir","/home/user/Documents/sharef"); // this is the main directory path that have shared
 
-echo '<pre>';
-print_r($files);
-echo '</pre>';
+function grabFileAndDirectories($nextDir){
+
+    $files = scandir(rootDir.$nextDir); // grabing all files in the directory
+    $files = array_diff($files,array('.','..')); // removing extra dots
+    return array_values($files);
+}
+
+
+function printListOfDirectoriesAndFiles($listOfFilesAndDirectories){
+
+    echo '<h3>All Directories and Files:</h3>'; // Print Heading 
+    echo '<pre>';
+    $fileCountIndex= 0;
+    foreach ($listOfFilesAndDirectories as$eachFile){
+        $fileCountIndex += 1;
+        echo "$fileCountIndex. ";    
+        echo($eachFile);
+        echo '<br>';
+    }
+    echo '</pre>';
+} 
+
+
+// Main Function:
+$files= grabFileAndDirectories('');
+printListOfDirectoriesAndFiles($files)
 ?> 
 
 
