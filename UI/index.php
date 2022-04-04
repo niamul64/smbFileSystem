b658a0d4e35538388cd0e721dddcf0b8a61cb8b3
Niamul Hasan <48917799+niamul64@users.noreply.github.com>
2022-04-04 15:42:03 +0600

    UI design

--- UI/index.php
+++ UI/index.php
@@ -4,9 +4,55 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    
+    <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+
+    <title>File Explorer</title>
+ 
 </head>
 <body>
+<body class="bg-light">
     
+<!-- container-fluid class start -->
+    <div class="container-fluid"> 
+        <main>
+            <div class=" text-center">
+            <h2>File Explorer</h2>
+            
+            </div>
+        </main>
+
+<!-- form start -->
+        <div class="border p-1 container-xl">
+        <p class="p-1 d-flex justify-content-center bg-white text-dark">Create directory/file or upload a file in the current path:</p>
+            <div class="row p-1">
+                <div class="col-sm-6">col-sm-6</div>
+                <div class="col-sm-5">col-sm-5</div>
+            </div>
+        </div>
+<!-- form end -->
+
+      </div>
+<!-- container-fluid class end-->
+
+
+      <!-- footer start -->
+      <footer class="pt-5 text-muted text-center text-small fixed-bottom">
+        <p class="mb-1">&copy; 2005–2022 Brotecs Technologies Limited</p>
+        <ul class="list-inline">
+          <li class="list-inline-item"><a href="#">Privacy</a></li>
+          <li class="list-inline-item"><a href="#">Terms</a></li>
+          <li class="list-inline-item"><a href="#">Support</a></li>
+        </ul>
+      </footer>
+      <!-- footer start end-->
+   
+    
+    <script src="script.js"></script>
 </body>
 </html>
