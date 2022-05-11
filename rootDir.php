b124586e4f9f86422a1b93a837d09e383562c9bb
niamul64 <niamulhassan64@gmail.com>
2022-05-11 12:48:01 +0600

    delete unnecessary files

---
+++ rootDir.php
@@ -0,0 +1,5 @@
+<?php
+define("rootDir","/home/niamul/Documents/sharef"); // this is the main directory path that have shared
+
+define("zipForMultiDownload","/home/niamul/Documents"); // zip file create and download path
+?>
